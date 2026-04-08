107786e5d0d1c26bc43a4378b2b27c0d4be8855a
TNYASULU <bed-com-26-21>
2026-04-08 17:45:11 +0200

    Updatedb

--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 from sqlalchemy import Column, Integer, String, Enum, DateTime
 from sqlalchemy.sql import func
 from database import Base
-from database.db import get_db
 
 class User(Base):
     __tablename__ = "users"
@@ -14,13 +13,3 @@ class User(Base):
     role                = Column(Enum("student", "teacher"), nullable=False)
     registration_number = Column(String(50), nullable=True)
     created_at          = Column(DateTime, server_default=func.now())
-
-
-def get_user_by_email(email):
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
-    user = cursor.fetchone()
-    cursor.close()
-    db.close()
-    return user
